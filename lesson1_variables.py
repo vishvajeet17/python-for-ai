28e26830269907f28cec5e2c676eaf5a2ea21d4a
Vishvajeet Singh <vishu2005.singh@gmail.com>
2026-07-15 19:29:58 +0530

    Lesson 1: Variables

---
+++ lesson1_variables.py
@@ -0,0 +1,9 @@
+name= "Vishvajeet Singh"
+college= "MIT ADT University"
+branch="Electronics and Communication Engineering"
+goal="Become an AI Engineer"
+
+print(name)
+print(college)
+print(branch)
+print(goal)
