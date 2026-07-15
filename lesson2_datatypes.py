c0d0e2a9995cfef529ff475c2e375749a0420859
Vishvajeet Singh <vishu2005.singh@gmail.com>
2026-07-15 19:38:04 +0530

    Lesson 2 : data Types

---
+++ lesson2_datatypes.py
@@ -0,0 +1,18 @@
+
+
+name = "Vishvajeet Singh"      
+age = 21                       
+cgpa = 7.5                     
+placed = False                 
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Placed:", placed)
+
+print()
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(placed))
